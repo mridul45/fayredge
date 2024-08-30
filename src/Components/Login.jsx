d3ad815784e0189e645be9fd4243b672c6dd98ff
mridul45 <mridulsinghal474@gmail.com>
2024-08-30 18:22:21 +0530

    Changes

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -4,27 +4,36 @@ import LoginImg from "/loginImg.png";
 function Login() {
   return (
     <div className="login_container">
+
       <div className="login_container_part1">
         <img className="img_class" src={LoginImg} />
       </div>
+
       <div className="login_container_part2">
+
         <div className="part2_logo">
           <img src="/logo.png" className="part2_logo" />
         </div>
 
         <div className="part2_form">
+
           welcome!!!
           <h2 className="h2">Sign in</h2>
+
           <form className="form">
+
             <div className="form_email">
               <label>Email</label>
               <input type="text" placeholder="Enter email" />
             </div>
+
             <div className="form_password">
               <label>Password</label>
               <input type="password" placeholder="Enter password" />
             </div>
+
           </form>
+          
         </div>
         <button className="btn">SIGN IN</button>
         <h5>
