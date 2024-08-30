0edb4543d572079540bfd62f3e3828d7ec884ee3
mridul45 <mridulsinghal474@gmail.com>
2024-08-30 18:03:50 +0530

    Changes

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,48 +1,38 @@
-import React from 'react'
-import "../Css/Login.css"
-import fayrEdgeLogo from '../images/fayredgeLogo.png'
-
+import "../Css/Login.css";
+import LoginImg from "/loginImg.png";
 
 function Login() {
   return (
     <div className="login_container">
-
-        <div className="login_container_part1">
-
+      <div className="login_container_part1">
+        <img className="img_class" src={LoginImg} />
+      </div>
+      <div className="login_container_part2">
+        <div className="part2_logo">
+          <img src="/logo.png" className="part2_logo" />
         </div>
 
-        <div className="login_container_part2">
-            
-            <div className="part2_logo">
-
+        <div className="part2_form">
+          welcome!!!
+          <h2 className="h2">Sign in</h2>
+          <form className="form">
+            <div className="form_email">
+              <label>Email</label>
+              <input type="text" placeholder="Enter email" />
             </div>
-
-            <div className="part2_form">
-                welcome
-                <h2>Sign in</h2>
-
-                <form className="form">
-                    <div className="form_email">
-                        <label>Email</label>
-                        <input type="text" placeholder='Enter email' />
-                    </div>
-
-                    <div className="form_password">
-                        <label>Password</label>
-                        <input type="password" placeholder='Enter password' />
-                    </div>
-
-                </form>
-                
+            <div className="form_password">
+              <label>Password</label>
+              <input type="password" placeholder="Enter password" />
             </div>
-
-            <button className="btn">SIGN IN</button>
-            <h5>Having trouble signining in? <a href="#">click here</a></h5>
-
+          </form>
         </div>
-
+        <button className="btn">SIGN IN</button>
+        <h5>
+          Having trouble signining in? <a href="#">click here</a>
+        </h5>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
