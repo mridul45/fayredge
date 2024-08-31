acca728f7e678983a1cf70aa94026a3d5677c713
AmitSutradhar001 <sutradharamit001@gmail.com>
2024-08-31 18:00:17 +0530

    app route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import Login from "./Components/Login";
+import "./App.css";
 
 function App() {
   return (
